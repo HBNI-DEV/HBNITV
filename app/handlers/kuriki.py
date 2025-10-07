c14c434b2802b82b1101ace9c72623634ebbe8f4
Jared Gross <jaredgrozz@gmail.com>
2025-10-06 19:21:32 -0500

    Update CORS headers in KurikiBaseHandler to allow additional methods and credentials

--- app/handlers/kuriki.py
+++ app/handlers/kuriki.py
@@ -7,8 +7,9 @@ from app.handlers.core.base import BaseHandler
 class KurikiBaseHandler(BaseHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "Content-Type")
-        self.set_header("Access-Control-Allow-Methods", "GET")
+        self.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        self.set_header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+        self.set_header("Access-Control-Allow-Credentials", "true")
         self.set_header("Content-Type", "application/json")
 
     def options(self):
